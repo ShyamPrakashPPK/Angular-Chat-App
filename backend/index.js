f9d6f16fdd450253ef8b2484042c5be2188a5849
Shyam Prakash <shyamprakash0019@gmail.com>
2023-06-07 21:04:05 +0530

    inital commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import http from "http";
+import { Server } from 'socket.io';
+
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
+
+io.on('connection', (socket) => {
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        socket.broadcast.to(data.room).emit('user joined');
+    });
+
+    socket.on('message', (data) => {
+        io.in(data.room).emit('new message', { user: data.user, message: data.message });
+    });
+})
