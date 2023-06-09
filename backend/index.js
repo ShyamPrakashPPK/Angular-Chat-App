77e8e51328bdd35990eae47480aa37ecc91ff8c0
Shyam Prakash <shyamprakash0019@gmail.com>
2023-06-09 23:32:57 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
     console.log(`Server started on port ${port}`);
